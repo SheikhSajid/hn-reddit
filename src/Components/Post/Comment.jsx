9c575afeb7705584eb33c44992e1848a2db9dd51
Sk Sajidul Kadir <sheikh.sajid522@gmail.com>
2020-08-03 20:33:49 +1000

    Show/Hide comments when threadline clicked

--- src/Components/Post/Comment.jsx
+++ src/Components/Post/Comment.jsx
@@ -1,4 +1,4 @@
-import React from 'react';
+import React, { useState, useCallback } from 'react';
 import distanceInWordsToNow from 'date-fns/formatDistanceToNow';
 import Comments from './Comments';
 
@@ -8,23 +8,40 @@ export default ({ comment }) => {
     addSuffix: true,
   });
 
+  const [childrenHidden, setChildrenHidden] = useState(false);
+
+  const showHideChildren = useCallback(() => {
+    if (childrenHidden) setChildrenHidden(false);
+    else setChildrenHidden(true);
+  }, [setChildrenHidden]);
+
   return (
     <div className="comment-container comment-container-outer">
-      <div className="threadline-container threadline-container-outer">
-        <div className="threadline"></div>
-      </div>
+      {!childrenHidden && (
+        <div
+          onClick={showHideChildren}
+          className="threadline-container threadline-container-outer"
+        >
+          <div className="threadline"></div>
+        </div>
+      )}
 
       <div className="comment-content">
-        <div className="post-metadata">
+        <div onClick={() => setChildrenHidden(false)} className="post-metadata">
+          {childrenHidden && '[+]'}
           {author} · {timestamp}
         </div>
 
-        <div
-          className="comment-body"
-          dangerouslySetInnerHTML={{ __html: text }}
-        ></div>
+        {!childrenHidden && (
+          <div
+            className="comment-body"
+            dangerouslySetInnerHTML={{ __html: text }}
+          ></div>
+        )}
 
-        {!!children.length && <Comments comments={children} />}
+        {!childrenHidden && !!children.length && (
+          <Comments comments={children} />
+        )}
       </div>
     </div>
   );
