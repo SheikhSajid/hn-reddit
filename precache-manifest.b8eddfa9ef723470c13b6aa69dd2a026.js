d82422cc1354f7bbab2d3ed0f99945c3447ceaf2
Sk Sajidul Kadir <sheikh.sajid522@gmail.com>
2020-08-18 22:17:49 +1000

    Updates

---
+++ precache-manifest.b8eddfa9ef723470c13b6aa69dd2a026.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "2855f513bb2ba67dfc845196f41c79e9",
+    "url": "/hn-reddit/index.html"
+  },
+  {
+    "revision": "0f634f62b3010d15703d",
+    "url": "/hn-reddit/static/css/main.388a21bb.chunk.css"
+  },
+  {
+    "revision": "09ef91270a7b92d7fc34",
+    "url": "/hn-reddit/static/js/2.63fb354a.chunk.js"
+  },
+  {
+    "revision": "e88a3e95b5364d46e95b35ae8c0dc27d",
+    "url": "/hn-reddit/static/js/2.63fb354a.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "0f634f62b3010d15703d",
+    "url": "/hn-reddit/static/js/main.5b96bcf3.chunk.js"
+  },
+  {
+    "revision": "ce6f31fa43b38e6d176e",
+    "url": "/hn-reddit/static/js/runtime-main.9da80c95.js"
+  }
+]);
--- service-worker.js
+++ service-worker.js
@@ -14,7 +14,7 @@
 importScripts("https://storage.googleapis.com/workbox-cdn/releases/4.3.1/workbox-sw.js");
 
 importScripts(
-  "/sheikhsajid.github.io/hn-reddit/precache-manifest.bfae1215ad4d24a4ee2f91700e786593.js"
+  "/hn-reddit/precache-manifest.b8eddfa9ef723470c13b6aa69dd2a026.js"
 );
 
 self.addEventListener('message', (event) => {
@@ -33,7 +33,7 @@ workbox.core.clientsClaim();
 self.__precacheManifest = [].concat(self.__precacheManifest || []);
 workbox.precaching.precacheAndRoute(self.__precacheManifest, {});
 
-workbox.routing.registerNavigationRoute(workbox.precaching.getCacheKeyForURL("/sheikhsajid.github.io/hn-reddit/index.html"), {
+workbox.routing.registerNavigationRoute(workbox.precaching.getCacheKeyForURL("/hn-reddit/index.html"), {
   
   blacklist: [/^\/_/,/\/[^\/?]+\.[^\/]+$/],
 });
