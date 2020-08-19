ad920f63f2f77cd8f8f644a6c64ba55e666ce279
Sk Sajidul Kadir <sheikh.sajid522@gmail.com>
2020-08-19 10:18:44 +1000

    Format getTopPosts.js using prettier

--- src/utils/getTopPosts.js
+++ src/utils/getTopPosts.js
@@ -8,45 +8,47 @@ export function useTopPosts(noOfPostsToFetch) {
   const [topPosts, setTopPosts] = useState([]);
   const [loading, setLoading] = useState(true);
 
-  const postIds = useRef([])
+  const postIds = useRef([]);
 
   // Fetch the IDs of the Top Stories
   useEffect(() => {
     fetch('https://hacker-news.firebaseio.com/v0/topstories.json')
-      .then(res => res.json())
-      .then(res => {
+      .then((res) => res.json())
+      .then((res) => {
         postIds.current = res;
 
         // Fetch the stories
         fetchPostContent(noOfPostsToFetch);
       });
-      
+
     // postIds.current = dummyPostIds;
     // setTopPosts(dummyPosts);
     // postsFetchedSoFar += dummyPosts.length;
     // setLoading(false);
   }, []);
-  
+
   async function fetchPostContent(noOfPostsToFetch, startIndex) {
     setLoading(true);
-    startIndex = (startIndex === undefined) ? postsFetchedSoFar : startIndex;
+    startIndex = startIndex === undefined ? postsFetchedSoFar : startIndex;
     const reqs = [];
-    
+
     for (let i = startIndex; i < startIndex + noOfPostsToFetch; i++) {
       const id = postIds.current[i];
-      
-      const req = fetch(`https://hacker-news.firebaseio.com/v0/item/${id}.json`);
+
+      const req = fetch(
+        `https://hacker-news.firebaseio.com/v0/item/${id}.json`
+      );
       reqs.push(req);
     }
 
     const responses = await Promise.all(reqs);
-    const jsonPromises = responses.map(res => res.json());
+    const jsonPromises = responses.map((res) => res.json());
     const posts = await Promise.all(jsonPromises);
     setLoading(false);
-    
+
     setTopPosts([...topPosts, ...posts]);
     postsFetchedSoFar += noOfPostsToFetch;
-    
+
     return posts;
   }
 
